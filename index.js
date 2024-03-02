5c909c76a8dea0fcc9f32aefb0fffd8d90e83e41
Deb Kumar Das <dasdebkumar48@gmail.com>
2024-03-02 12:22:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll(".panel");
+
+function panelOpen() {
+  this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener("click", panelOpen));
+
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
